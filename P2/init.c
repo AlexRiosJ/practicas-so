c33fbc3d4f6e2c7e2336c0e1a76c67a18dac74ad
Alejandro Ríos Jasso <jassoalex97@hotmail.com>
2019-06-06 14:09:23 -0500

    Agregando cambios a init para lanzar 6 getty

--- P2/init.c
+++ P2/init.c
@@ -1,8 +1,24 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <unistd.h>
+#include <sys/wait.h>
+
+#define CHILDREN 6
 
 int main() {
-    // Poner lo necesario para que funcione...
+    int pid;
+    int i;
+
+    for(i = 0; i < CHILDREN; i++){
+        pid = fork();
+        int status;
+        if(pid == 0){
+            execlp("xterm", "xterm", "-e", "./getty", NULL);
+            exit(0);
+        } else {
+			wait(&status);
+        }
+    }
+
     return 0;
 }
