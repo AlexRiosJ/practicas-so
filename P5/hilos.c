10522956e4dcb15c93aedd93334b05f95a1e27b1
Alejandro Rios Jasso <jassoalex97@hotmail.com>
2019-06-20 15:08:35 -0500

    Añadiendo enfoque con procesos y probando

--- P5/hilos.c
+++ P5/hilos.c
@@ -24,18 +24,23 @@ void *hilo1(void *arg)
     {
         l = 1;
         do { atomic_xchg(l, g); } while (l != 0);
+        
         // Inicia sección Crítica
         printf("Entra %s", pais[i]);
+        
         fflush(stdout);
         sleep(rand() % 3);
         printf("- %s Sale\n", pais[i]);
+        
         // Termina sección Crítica
         g = 0;
         l = 1;
+        
         // Espera aleatoria fuera de la sección crítica
         sleep(rand() % 3);
     }
 }
+
 int main()
 {
     pthread_t tid[3];
@@ -44,12 +49,14 @@ int main()
     int i;
     void *thread_result;
     srand(getpid());
+    
     // Crea los hilos
     for (i = 0; i < 3; i++)
     {
         args[i] = i;
         res = pthread_create(&tid[i], NULL, hilo1, (void *)&args[i]);
     }
+    
     // Espera que terminen los hilos
     for (i = 0; i < 3; i++)
         res = pthread_join(tid[i], &thread_result);
