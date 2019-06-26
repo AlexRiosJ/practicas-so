abde298bda8559d9f4a5c5804b3a4f73f3aebb8b
Alfredo Rodriguez <alfredordgz98@gmail.com>
2019-06-26 10:55:28 -0600

    Cambio del main para lo solicitado

--- P5/procesos.c
+++ P5/procesos.c
@@ -5,14 +5,15 @@
 #include <sys/shm.h>
 #include <sys/wait.h>
 
+// FUNCIÓN DE HW PARA EL INTERCAMBIO DE PROCESOS 
 #define atomic_xchg(A, B) __asm__ __volatile__( \
     " lock xchg %1,%0 ;\n"                      \
     : "=ir"(A)                                  \
     : "m"(B), "ir"(A));
-#define CICLOS 10
 
+// VARIABLES GLOBALES ESTABLECIDAS
+#define CICLOS 10
 char *pais[3] = {"Peru", "Bolvia", "Colombia"};
-int *g;
 
 /* CÓDIGO AÑADIDO  DE LAS COLAS*/
 typedef struct _QUEUE {
@@ -59,13 +60,24 @@ typedef struct SEMAPHORE{
     unsigned int bloqueados;
     QUEUE cola_de_bloqueados;
 }SEMAPHORE;
+
+SEMAPHORE *semaforo;
+
 /*  INICIO DE LAS FUNCIONES DEL SEMÁFORO */
+
+void initsem(SEMAPHORE s,int cuenta){
+    s.contador = cuenta;
+    s.bloqueados = 0;
+    _initqueue(&s.cola_de_bloqueados);
+}
+
 void waitsem(SEMAPHORE s){
     if(s.contador == 0){
         int procesoActual = getpid();
         _enqueue(&s.cola_de_bloqueados,procesoActual); // Se encola en la lista de bloqueados
         s.bloqueados++;
         // AÑADIR CÓDIGO PARA QUE SE DUERMA ESTE PROCESO
+        // atomic_xchg();
     }else{
         s.contador--;
     }
@@ -77,6 +89,7 @@ void signalsem(SEMAPHORE s){
     }else{
         int procesoPorDespertar = _dequeue(&s.cola_de_bloqueados);
         // AÑADIR CÓDIGO PARA QUE SE VUELVA A ACTIVAR ESE PROCESO
+        // atomic_xchg();
         s.bloqueados--;
     }
 }
@@ -85,24 +98,20 @@ void signalsem(SEMAPHORE s){
 void proceso(int i)
 {
     int k;
-    int l;
-    for (k = 0; k < CICLOS; k++)
-    {
-        l = 1;
-        do { atomic_xchg(l, *g); } while (l != 0);
-        printf("Entra %s", pais[i]);
-        
-        fflush(stdout);
-        sleep(rand() % 3);
-        printf("- %s Sale\n", pais[i]);
-        
-        l = 1;
-        *g = 0;
-        
-        // Espera aleatoria fuera de la sección crítica
-        sleep(rand() % 3);
+    for (k=0;k<CICLOS;k++){
+    // Llamada waitsem implementada en la parte 3
+    waitsem(*semaforo);
+    printf("Entra %s ",pais[i]);
+    fflush(stdout);
+    sleep(rand()%3);
+    printf("-%s Sale\n",pais[i]);
+    // Llamada waitsignal implementada en la parte 3
+    signalsem(*semaforo);
+    // Espera aleatoria fuera de la sección crítica
+    sleep(rand()%3);
     }
-    exit(0); // Termina el proceso
+    exit(0);
+    // Termina el proceso
 }
 
 int main()
@@ -115,7 +124,7 @@ int main()
     void *thread_result;
     
     // Solicitar memoria compartida
-    shmid = shmget(0x1234, sizeof(g), 0666 | IPC_CREAT);
+    shmid = shmget(0x1234, sizeof(SEMAPHORE), 0666 | IPC_CREAT);
     if (shmid == -1)
     {
         perror("Error en la memoria compartida\n");
@@ -123,14 +132,15 @@ int main()
     }
     
     // Conectar la variable a la memoria compartida
-    g = shmat(shmid, NULL, 0);
-    if (g == NULL)
+    semaforo = shmat(shmid, NULL, 0);
+    if (semaforo == NULL)
     {
         perror("Error en el shmat\n");
         exit(2);
     }
     
-    *g = 0;
+    //Inicializando semáforo
+    initsem(*semaforo,1);
     srand(getpid());
     
     for (i = 0; i < 3; i++)
@@ -145,5 +155,5 @@ int main()
         pid = wait(&status);
     
     // Eliminar la memoria compartida
-    shmdt(g);
+    shmdt(semaforo);
 }
