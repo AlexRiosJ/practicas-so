4b597db72131b12b553fb3e32203ff5629b52c13
Alfredo Rodriguez <alfredordgz98@gmail.com>
2019-06-26 12:23:59 -0600

    Inicio P6 semáforos UNIX

---
+++ P6/procesoUNIX.c
@@ -0,0 +1,61 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <unistd.h>
+#include "semaphores.h"
+
+							
+#define CICLOS 10
+
+char *pais[3]={"Peru","Bolvia","Colombia"};
+
+int semaforo;
+
+void proceso(int i)
+{
+	int k;
+
+	for(k=0;k<CICLOS;k++)
+	{
+		semwait(semaforo);	
+		// Inicia sección Crítica
+		printf("Entra %s",pais[i]);
+		fflush(stdout);
+		sleep(rand()%3);
+		printf("- %s Sale\n",pais[i]);
+		// Termina sección Crítica
+		semsignal(semaforo);
+		
+		// Espera aleatoria fuera de la sección crítica
+		sleep(rand()%3);
+	}
+    exit(0);   // Termina el proceso
+}
+
+int main()
+{
+	int pid;
+    int status;
+	int args[3];
+	int i;
+
+	srand(getpid());
+
+    // Inicializando un semáforo desde el SO para recibir a los procesos
+	semaforo = seminit(0x1234,2);
+
+	// Crea los hilos
+	for(i=0;i<3;i++)
+	{
+        // Crea un nuevo proceso hijo que ejecuta la función proceso()
+		pid = fork();
+        if(pid == 0)
+            proceso(i);
+	}
+
+	// Espera que terminen los hilos
+	for(i=0;i<3;i++)
+        pid = wait(&status);
+		
+    // Borrando el semáforo creado
+	semdelete(semaforo);
+}
