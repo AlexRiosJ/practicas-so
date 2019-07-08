43c4119cace6be70718d5dda05dae1f7a935eb67
Alejandro Rios Jasso <jassoalex97@hotmail.com>
2019-07-08 17:48:56 -0500

    Codigo añadido solo falta agregar funciones

--- P7/todo/pagefaultVM.c
+++ P7/todo/pagefaultVM.c
@@ -9,9 +9,9 @@
 
 #define NUMPROCS 4
 #define PAGESIZE 4096
-#define PHISICALMEMORY 12*PAGESIZE 
-#define TOTFRAMES PHISICALMEMORY/PAGESIZE
-#define RESIDENTSETSIZE PHISICALMEMORY/(PAGESIZE*NUMPROCS)
+#define PHISICALMEMORY 12 * PAGESIZE
+#define TOTFRAMES PHISICALMEMORY / PAGESIZE
+#define RESIDENTSETSIZE PHISICALMEMORY / (PAGESIZE * NUMPROCS)
 
 extern char *base;
 extern int framesbegin;
@@ -22,7 +22,6 @@ extern int ptlr;
 extern struct SYSTEMFRAMETABLE *systemframetable;
 extern struct PROCESSPAGETABLE *ptbr;
 
-
 int getfreeframe();
 int searchvirtualframe();
 int getfifo();
@@ -30,62 +29,80 @@ int getfifo();
 int pagefault(char *vaddress)
 {
     int i;
-    int frame,vframe; 
+    int frame, vframe;
     long pag_a_expulsar;
     int fd;
     char buffer[PAGESIZE];
     int pag_del_proceso;
 
     // A partir de la dirección que provocó el fallo, calculamos la página
-    pag_del_proceso=(long) vaddress>>12;
-
+    pag_del_proceso = (long)vaddress >> 12;
 
     // Si la página del proceso está en un marco virtual del disco
+    if ((ptbr + pag_del_proceso)->framenumber >= (systemframetablesize + framesbegin))
     {
-
-		// Lee el marco virtual al buffer
-
+        // Lee el marco virtual al buffer
+        readblock(buffer, (ptbr + pag_del_proceso)->framenumber);
         // Libera el frame virtual
+        systemframetable[(ptbr + pag_del_proceso)->framenumber].assigned = 0;
     }
 
-
     // Cuenta los marcos asignados al proceso
-    i=countframesassigned();
+    i = countframesassigned();
 
     // Si ya ocupó todos sus marcos, expulsa una página
-    if(i>=RESIDENTSETSIZE)
+    if (i >= RESIDENTSETSIZE)
     {
-		// Buscar una página a expulsar
-		
-		// Poner el bitde presente en 0 en la tabla de páginas
-        
+        // Buscar una página a expulsar
+        pag_a_expulsar = getfifo();
+        // Poner el bit de presente en 0 en la tabla de páginas
+        (ptbr + pag_a_expulsar)->presente = 0;
         // Si la página ya fue modificada, grábala en disco
+        if((ptbr + pag_a_expulsar)->modificado)
         {
-			// Escribe el frame de la página en el archivo de respaldo y pon en 0 el bit de modificado
+            // Escribe el frame de la página en el archivo de respaldo y pon en 0 el bit de modificado
+            saveframe((ptbr + pag_a_expulsar)->framenumber);
+            (ptbr + pag_a_expulsar)->modificado = 0;
         }
-		
+
         // Busca un frame virtual en memoria secundaria
-		// Si no hay frames virtuales en memoria secundaria regresa error
-		{
-            return(-1);
+        vframe = searchvirtualframe();
+
+        // Si no hay frames virtuales en memoria secundaria regresa error
+        if (vframe == -1)
+        {
+            return (-1);
         }
         // Copia el frame a memoria secundaria, actualiza la tabla de páginas y libera el marco de la memoria principal
+        copyframe((ptbr + pag_a_expulsar)->framenumber, vframe);
+        frame = (ptbr + pag_a_expulsar)->framenumber;
+        systemframetable[frame].assigned = 0;
+        (ptbr + pag_a_expulsar)->framenumber = vframe;
     }
 
     // Busca un marco físico libre en el sistema
-	// Si no hay marcos físicos libres en el sistema regresa error
+    frame = getfreeframe();
+    // Si no hay marcos físicos libres en el sistema regresa error
+    if(frame == -1)
     {
-        return(-1); // Regresar indicando error de memoria insuficiente
+        return (-1); // Regresar indicando error de memoria insuficiente
     }
 
     // Si la página estaba en memoria secundaria
+    if(!(ptbr + pag_del_proceso)->presente)
     {
         // Cópialo al frame libre encontrado en memoria principal y transfiérelo a la memoria física
+        copyframe((ptbr + pag_del_proceso)->framenumber, frame);
+        writeblock(buffer, frame);
+        loadframe(frame);
     }
-   
-	// Poner el bit de presente en 1 en la tabla de páginas y el frame 
 
+    // Poner el bit de presente en 1 en la tabla de páginas y el frame
+    (ptbr + pag_del_proceso)->presente = 1;
+    (ptbr + pag_del_proceso)->framenumber = frame;
 
-    return(1); // Regresar todo bien
+    return (1); // Regresar todo bien
 }
 
+
+// Implementar funciones
